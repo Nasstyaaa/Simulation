36c6f4d57c7eb319289435b92a168e03eae9980d
Nastya <Luk-2004@mail.ru>
2024-07-01 20:51:02 +0300

    add action 'initMap'

--- src/Simulation/Map.java
+++ src/Simulation/Map.java
@@ -6,25 +6,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Map {
-    ArrayList<ArrayList<Entity>> mainCollectionOfLocation;
+    private ArrayList<ArrayList<Entity>> mainCollectionOfLocation;
+    private int lengthX;
+    private int lengthY;
 
     public ArrayList<ArrayList<Entity>> getMainCollectionOfLocation() {
         return mainCollectionOfLocation;
     }
 
-    public Map(int lengthByX, int lengthByY) {
-        mainCollectionOfLocation = new ArrayList<>(lengthByX);
-        for(int i = 0; i < lengthByX; i++){
-            mainCollectionOfLocation.add(new ArrayList<>(Collections.nCopies(lengthByY, null)));
-        }
+    public int getLengthX() {
+        return lengthX;
     }
 
-// TODO добавить проверки
-    public void addObjectsOnMap(ArrayList<Entity> entitiesCollection){
-       for(Entity entity: entitiesCollection){
-           int x = entity.getLocationByX();
-           int y = entity.getLocationByY();
-           mainCollectionOfLocation.get(x).set(y, entity);
-       }
+    public int getLengthY() {
+        return lengthY;
+    }
+
+    public Map(int lengthByX, int lengthByY) {
+        mainCollectionOfLocation = new ArrayList<>(lengthByX);
+        for (int i = 0; i < lengthByX; i++) {
+            mainCollectionOfLocation.add(new ArrayList<>(Collections.nCopies(lengthByY, null)));
+        }
+        lengthX = lengthByX;
+        lengthY = lengthByY;
+    }
+
+    public void addObjectsOnMap(ArrayList<Entity> entitiesCollection) {
+        for (Entity entity : entitiesCollection) {
+            int x = entity.getLocationByX();
+            int y = entity.getLocationByY();
+            if (mainCollectionOfLocation.get(x).get(y) == null) {
+                mainCollectionOfLocation.get(x).set(y, entity);
+            } else {
+                while (mainCollectionOfLocation.get(x).get(y) != null) {
+                    x = (int) (Math.random() * (lengthX));
+                    y = (int) (Math.random() * (lengthY));
+                }
+                mainCollectionOfLocation.get(x).set(y, entity);
+            }
+        }
     }
 }
