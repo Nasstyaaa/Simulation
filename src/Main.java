5900e8184a8c7c2f9459a7e46e99b7df09eb18fe
Nastya <Luk-2004@mail.ru>
2024-07-31 12:32:43 +0300

    fixed render

--- src/Main.java
+++ src/Main.java
@@ -8,31 +8,5 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        MapWorld map = new MapWorld(5, 5);
-
-        Herbivore herbivore = new Herbivore();
-        ArrayList<Herbivore> arrayOfHerbivore = herbivore.createObjectOnMap(map);
-
-        Predator predator = new Predator();
-        predator.createObjectOnMap(map);
-
-        Grass grass = new Grass();
-        grass.createObjectOnMap(map);
-
-        Rock rock = new Rock();
-        rock.createObjectOnMap(map);
-
-        Tree tree = new Tree();
-        tree.createObjectOnMap(map);
-
-        Set<String> keys = map.getMainCollectionOfLocation().keySet();
-        System.out.println("Keys: " + keys);
-
-        for (String key : keys) {
-            Entity entity = map.getMainCollectionOfLocation().get(key);
-            if (entity != null) {
-                System.out.println("Entity at " + key + ": " + entity.getClass());
-            }
         }
     }
-}
